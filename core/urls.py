e60b34942af473ed8022f08a494f286e6b8f7533
Kenneth Cox <damitwhy@gmail.com>
2024-08-15 14:05:01 +0000

    urls's views, home.html

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from . import views
 from .views import home
 
-path [('', views.home.as_view(), name='home'),
+urlpatterns = [('', views.home.as_view(), name='home'),
       
 ]
