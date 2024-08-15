04c8798b8c4ac43ef6fdc0da1ea7aedede52060c
Kenneth Cox <damitwhy@gmail.com>
2024-08-15 14:47:46 +0000

    Project URLs.py points to core views home now

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 from .views import home
 
-urlpatterns = [('', views.home.as_view(), name='home'),
+urlpatterns = [
+    path('', views.home.as_view(), name='home'),
+               
+],
       
-]
