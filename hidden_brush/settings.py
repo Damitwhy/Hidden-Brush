ee2d18f7629eb65ede2d18f51fb26a7e8ad6008c
Kenneth Cox <damitwhy@gmail.com>
2024-08-15 17:14:47 +0000

    cheking HeroKu fault

--- hidden_brush/settings.py
+++ hidden_brush/settings.py
@@ -30,7 +30,7 @@ TIME_ZONE = 'GB'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','.ws.codeinstitute-ide.net','.herokuapp.com']
 
