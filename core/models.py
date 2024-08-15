7294f40ea66979ad43f389fea646d1df031588ff
Kenneth Cox <damitwhy@gmail.com>
2024-08-15 12:43:26 +0000

    Initial Django installed

---
+++ core/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+
+class User(AbstractUser):
+    """
+    User model representing a user in the system.
+    Attributes:
+        is_admin (bool): Indicates whether the user is an admin or not. Default is False.
+    """    
+    is_admin = models.BooleanField(default=False)
