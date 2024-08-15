147baad096842ea262575ac50d5c3acb42c08e1a
Kenneth Cox <damitwhy@gmail.com>
2024-08-15 14:50:13 +0000

    Project URLs.py points to core views home now

--- hidden_brush/urls.py
+++ hidden_brush/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from core import views
+from django.urls import path, include
+
 
 urlpatterns = [
+    path('', include('core.urls'), name='core_urls'),
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    
 ]
