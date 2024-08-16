ff3c4608b64f9e87deb6ad413b256818303548bb
Kenneth Cox <damitwhy@gmail.com>
2024-08-16 00:15:14 +0000

    rewrote env.py changing the Cloudinary addition, added cloudinary import to models.py

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+from cloudinary.models import CloudinaryField
 
 class User(AbstractUser):
     """
