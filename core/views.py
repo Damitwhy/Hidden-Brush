0214342674dd3525fc8f314098e3fe89207d7129
Kenneth Cox <damitwhy@gmail.com>
2024-08-16 10:20:16 +0000

    updated views.py to include a detailed view to comment, created comment form in forms.py, updated urls.py to include url to image_detail.html, created model for Comments

--- core/views.py
+++ core/views.py
@@ -7,8 +7,26 @@ from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from .models import Comment
+from .forms import CommentForm
 
 
 def home(request):
     # Renders the home page template
     return render(request, 'core/home.html')
+
+@login_required
+def image_detail(request, image_id):
+    comments = Comment.objects.filter(image_id=image_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.image_id = image_id
+            comment.save()
+            return redirect('image_detail', image_id=image_id)
+    else:
+        form = CommentForm()
+    return render(request, 'image_detail.html', {'image_id': image_id, 'comments': comments, 'form': form})
