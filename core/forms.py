727d360ba5ffd8f54c249fe31b419c081af4462a
Ahmed Mohamed <ahmedtarekaha@gmail.com>
2024-08-17 10:14:56 +0000

    added login logout register templates and added login logout register views and fix the urls for them

--- core/forms.py
+++ core/forms.py
@@ -1,13 +1,22 @@
 from django import forms
 from .models import Comment, Image
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User  # Point to your custom User model
+        fields = ('username', 'email', 'password1', 'password2')
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['text']
-        
+
+
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ['image', 'title', 'description']
-
